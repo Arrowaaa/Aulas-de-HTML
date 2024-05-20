811d71095239e5b37a1c6728d605348f7c38a0b7
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-05-20 11:59:21 -0300

    Aula do dia 20/05/2024 Animações no banners com js

---
+++ Aula13HTML/js/script.js
@@ -0,0 +1,38 @@
+// var = "varival para usar na pagina completa"
+// let = "varival para usar no contexto aplicado/ variavel temporaria/ descartavel"
+// const = "varival para usar FIXAMENTE (sem alteração/variavel global)"
+
+let indexSlide = 0
+
+function showSlide(index) {
+    const carrossel = document.querySelector(".carrossel");
+
+    const totalSlide = 7;
+
+    if(index >= totalSlide){
+        indexSlide = 0;
+
+    } else if (index < 0){
+        indexSlide = totalSlide - 1;
+
+    } else{
+        indexSlide = index;
+    }
+
+
+
+    const offset = -indexSlide * 100;
+    carrossel.style.transform = `translateX(${offset}%)`;
+}
+
+function anteriorSlide() {
+    indexSlide = indexSlide - 1;
+    showSlide(indexSlide);
+
+}
+
+function proximoSlide() {
+    indexSlide = indexSlide + 1;;
+    showSlide(indexSlide);
+
+}
